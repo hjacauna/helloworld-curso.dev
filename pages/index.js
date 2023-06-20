f5db37a41758cd7ac4b0ff8dd9be12ba67f3ec30
hjacauna <136808571+hjacauna@users.noreply.github.com>
2023-06-20 15:27:17 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Eu sei que é simples, tambem sei qual vai ser sua reação, mas acredite, é um primeiro passo para uma nova etapa, eu te amo</h1>
+}
+
+export default Home;
