ac90404f83ad721c03780b9ef25d5240a1dcf9c7
hjacauna <136808571+hjacauna@users.noreply.github.com>
2023-06-24 13:41:43 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Eu sei que é simples, tambem sei qual vai ser sua reação, mas acredite, é um primeiro passo para uma nova etapa, eu te amo!</h1>
+function Home() {
+  return (
+    <h1>
+      Eu sei que é simples, tambem sei qual vai ser sua reação, mas acredite, é
+      um primeiro passo para uma nova etapa, eu te amo!
+    </h1>
+  );
 }
 
 export default Home;
