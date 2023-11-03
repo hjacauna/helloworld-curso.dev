d5375dff8bec46f96dba41b2f65a26b6437cc239
hjacauna <136808571+hjacauna@users.noreply.github.com>
2023-11-03 22:28:44 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query(
+    "SELECT current_user, current_database()"
+  );
+  console.log(result.rows);
   response.status(200).json({ chave: "teste api" });
 }
-
 export default status;
